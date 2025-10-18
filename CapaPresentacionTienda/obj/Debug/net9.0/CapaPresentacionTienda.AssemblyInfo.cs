e908715d92ae1b5a2b8d64394d3f5deb742010e7
Bryan Romero <bryanromerof26@gmail.com>
2025-10-18 08:23:18 -0600

    se creo el proyecto y se trabajo en el modulo de administracion hasta tenerlo casi completado

--- CapaPresentacionTienda/obj/Debug/net9.0/CapaPresentacionTienda.AssemblyInfo.cs
+++ CapaPresentacionTienda/obj/Debug/net9.0/CapaPresentacionTienda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0372017b86446d07225e854bed13ef35a13cad49")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
