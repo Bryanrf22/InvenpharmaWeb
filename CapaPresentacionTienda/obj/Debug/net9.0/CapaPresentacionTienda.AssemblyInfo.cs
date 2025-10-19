1c991bf9fbf5129742619be22e0591e176fb2288
Bryan Romero <bryanromerof26@gmail.com>
2025-10-19 11:44:28 -0600

    Update static web assets and assembly information for CapaPresentacionAdmin and CapaPresentacionTienda
    - Added new static web assets for Bootstrap and custom scripts/styles in CapaPresentacionAdmin.
    - Updated assembly informational version in CapaPresentacionTienda from "1.0.0+0372017b86446d07225e854bed13ef35a13cad49" to "1.0.0+da8cbe77485eddc2f2ecc7e90b71f6917b8198d3".
    - Updated assembly info cache for CapaPresentacionTienda.

--- CapaPresentacionTienda/obj/Debug/net9.0/CapaPresentacionTienda.AssemblyInfo.cs
+++ CapaPresentacionTienda/obj/Debug/net9.0/CapaPresentacionTienda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0372017b86446d07225e854bed13ef35a13cad49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8cbe77485eddc2f2ecc7e90b71f6917b8198d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacionTienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
